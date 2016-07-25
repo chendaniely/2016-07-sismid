d12f6b179ce6cf018534b4d2ad8e670cf83ff902
Daniel Chen <Maveri2k@yahoo.com>
2016-07-25 15:38:38 -0700

    get data

---
+++ sismid_16-spatial_statistics/download_data.sh
@@ -0,0 +1,7 @@
+cd data
+
+wget http://faculty.washington.edu/jonno/SISMIDmaterial/wacounty.{shp,shx,dbf}
+wget http://faculty.washington.edu/jonno/SISMIDmaterial/watract.{shp,shx,dbf}
+wget http://faculty.washington.edu/jonno/SISMIDmaterial/scot.{shp,shx,dbf}
+
+cd ..
